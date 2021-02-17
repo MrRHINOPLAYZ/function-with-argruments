b496a814731622b37d5953837b4c8640b02066a6
MrRHINOPLAYZ <78490879+MrRHINOPLAYZ@users.noreply.github.com>
2021-02-17 19:59:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,fixedRect)){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+  
+  else {
+    movingRect.shapeColor = "green";
+  
+    fixedRect.shapeColor = "green"
+  }
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
